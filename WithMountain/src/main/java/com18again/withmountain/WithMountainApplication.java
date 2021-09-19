4375806a0d3acffd40d43dc54fbf6a3f33073741
EunhaKyeong <kyeongeh424@gmail.com>
2021-09-19 13:45:48 +0900

    First Commit!

---
+++ WithMountain/src/main/java/com18again/withmountain/WithMountainApplication.java
@@ -0,0 +1,13 @@
+package com18again.withmountain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithMountainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithMountainApplication.class, args);
+	}
+
+}
---
+++ WithMountain/src/test/java/com18again/withmountain/WithMountainApplicationTests.java
@@ -0,0 +1,13 @@
+package com18again.withmountain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithMountainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
