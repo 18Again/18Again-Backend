4985c384442b8596ee226c38901985c2639256a8
EunhaKyeong <kyeongeh424@gmail.com>
2021-09-22 11:24:29 +0900

    test 테이블 api 코드 작성

--- WithMountain/src/main/java/com/withmountain/again18/controller/TestController.java
+++ WithMountain/src/main/java/com/withmountain/again18/controller/TestController.java
@@ -1,13 +1,27 @@
 package com.withmountain.again18.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.withmountain.again18.domain.TestDTO;
+import com.withmountain.again18.mapper.TestMapper;
+
 @RestController
 public class TestController {
+	@Autowired
+	private TestMapper testMapper;
+	
 	@GetMapping("/api")
 	private String test() {
 		return "hello World!";
+	}
+	
+	@GetMapping("/api/testDB")
+	private List<TestDTO> testDB() {
+		return testMapper.selectTestList();
 	}
 
 }
