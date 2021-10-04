f1737ddbb2f7a3ab37778d32b9bd2d5343d7a9d8
서호진 <seohojin99@naver.com>
2021-10-04 22:24:05 +0900

    boardInsert_api

--- WithMountain/src/main/java/com/withmountain/again18/controller/BoardController.java
+++ WithMountain/src/main/java/com/withmountain/again18/controller/BoardController.java
@@ -1,10 +1,44 @@
 package com.withmountain.again18.controller;
 
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.withmountain.again18.domain.BoardDTO;
+import com.withmoutain.again18.service.BoardService;
 
+@RestController
 public class BoardController {
 	
+	@Autowired
+	private BoardService boardService;
+	
+	/*
+	@RequestMapping(value="/board",method=RequestMethod.GET)
+	public List<BoardDTO> openBoardList(){
+		return boardService.getBoardList();
+	}
 	
 	
+	@GetMapping(value = "/board/write.do")
+	public String openBoardWrite(@RequestParam(value = "idx", required = false) int id, Model model) {
+		if (id == 0) {
+			model.addAttribute("board", new BoardDTO());
+		} else {
+			BoardDTO board = boardService.getBoardDetail(id);
+			if (board == null) {
+				return "redirect:/board/list.do";
+			}
+			model.addAttribute("board", board);
+		}
+
+		return  "write/do";
+	}
+	*/
 }
